c4aa13033095708d35942d4741e086eac8d8d844
Himanshu Rana <himanshu.a.rana@gmail.com>
2018-08-24 23:17:29 +0530

    passing params as props part 2

--- src/parts/PartInfo.vue
+++ src/parts/PartInfo.vue
@@ -14,7 +14,18 @@ import parts from '../data/parts';
 
 export default {
   name: 'PartInfo',
-  props: ['partType', 'id'],
+  // props: ['partType', 'id'],
+  props: {
+    partType: {
+      type: String,
+    },
+    id: {
+      type: [Number, String],
+      validator(value) {
+        return Number.isInteger(Number(value));
+      },
+    },
+  },
   computed: {
     part() {
       // const partType = this.$route.params.partType;
