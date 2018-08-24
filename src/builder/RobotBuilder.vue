d72346650e3c43a3c620f4a6adaca5e79f1f5bd6
Himanshu Rana <himanshu.a.rana@gmail.com>
2018-08-24 22:13:26 +0530

    Module 4 enabling inter-component communication completed

--- src/builder/RobotBuilder.vue
+++ src/builder/RobotBuilder.vue
@@ -1,56 +1,94 @@
 <template>
   <div class="content">
-    <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
+       <div class="preview">
+         <CollapsibleSection>
+      <div class="preview-content">
+        <div class="top-row">
+          <img :src="selectedRobot.head.src"/>
+        </div>
+        <div class="middle-row">
+          <img :src="selectedRobot.leftArm.src" class="rotate-left"/>
+          <img :src="selectedRobot.torso.src"/>
+          <img :src="selectedRobot.rightArm.src" class="rotate-right"/>
+        </div>
+        <div class="bottom-row">
+          <img :src="selectedRobot.base.src"/>
+        </div>
+      </div>
+         </CollapsibleSection>
+      <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
+    </div>
+    <!-- <button class="add-to-cart" @click="addToCart()">Add to Cart</button> -->
     <div class="top-row">
+      <PartSelector
+      :parts="availableParts.heads"
+      position="top"
+      @partSelected="part => selectedRobot.head = part" />
       <!-- <div class="top part" :style="headBorderStyle"> -->
       <!-- <div class="top part" :class="{'sale-border': selectedRobot.head.onSale}"> -->
       <!-- <div class="top part" :class="[saleBorderClass]"> -->
-      <div :class="[saleBorderClass, 'top', 'part']">
+      <!-- <div :class="[saleBorderClass, 'top', 'part']">
         <div class="robot-name">
           {{selectedRobot.head.title}}
-          <span v-if="selectedRobot.head.onSale" class="sale">Sale! </span>
+          <span v-if="selectedRobot.head.onSale" class="sale">Sale! </span> -->
           <!-- <span v-show="selectedRobot.head.onSale" class="sale">Sale! </span> -->
-        </div>
+        <!-- </div> -->
         <!-- <img src="./images/head-big-eye.png" title="head" /> -->
         <!-- <img v-bind:src="availableParts.heads[selectedHeadIndex].src" title="head" /> -->
         <!-- <img :src="availableParts.heads[selectedHeadIndex].src" title="head" /> -->
-        <img :src="selectedRobot.head.src" title="head" />
+        <!-- <img :src="selectedRobot.head.src" title="head" /> -->
         <!-- <button class="prev-selector" v-on:click="selectPreviousHead()">&#9668;</button> -->
-        <button class="prev-selector" @click="selectPreviousHead()">&#9668;</button>
+        <!-- <button class="prev-selector" @click="selectPreviousHead()">&#9668;</button>
         <button class="next-selector" v-on:click="selectNextHead()">&#9658;</button>
-      </div>
+      </div> -->
     </div>
     <div class="middle-row">
-      <div class="left part">
+      <PartSelector
+      :parts="availableParts.arms"
+      position="left"
+      @partSelected="part => selectedRobot.leftArm = part" />
+      <PartSelector
+      :parts="availableParts.torsos"
+      position="center"
+      @partSelected="part => selectedRobot.torso = part" />
+      <PartSelector
+      :parts="availableParts.arms"
+      position="right"
+      @partSelected="part => selectedRobot.rightArm = part" />
+      <!-- <div class="left part"> -->
         <!-- <img src="./images/arm-articulated-claw.png" title="left arm" /> -->
         <!-- <img v-bind:src="availableParts.arms[selectedLeftArmIndex].src" title="head" /> -->
-        <img v-bind:src="selectedRobot.leftArm.src" title="left arm" />
+        <!-- <img v-bind:src="selectedRobot.leftArm.src" title="left arm" />
         <button class="prev-selector" v-on:click="selectPreviousLeftArm()">&#9650;</button>
         <button class="next-selector" v-on:click="selectNextLeftArm()">&#9660;</button>
-      </div>
-      <div class="center part">
+      </div> -->
+      <!-- <div class="center part"> -->
         <!-- <img src="./images/torso-flexible-gauged.png" title="left arm" /> -->
         <!-- <img v-bind:src="availableParts.torsos[selectedTorsosIndex].src" title="head" /> -->
-        <img v-bind:src="selectedRobot.torso.src" title="torso" />
+        <!-- <img v-bind:src="selectedRobot.torso.src" title="torso" />
         <button class="prev-selector" v-on:click="selectPreviousTorso()">&#9650;</button>
         <button class="next-selector" v-on:click="selectNextTorso()">&#9660;</button>
-      </div>
-      <div class="right part">
+      </div> -->
+      <!-- <div class="right part"> -->
         <!-- <img src="./images/arm-dual-claw.png" title="left arm" /> -->
         <!-- <img v-bind:src="availableParts.arms[selectedRightArmIndex].src" title="head" /> -->
-        <img v-bind:src="selectedRobot.rightArm.src" title="right arm" />
+        <!-- <img v-bind:src="selectedRobot.rightArm.src" title="right arm" />
         <button class="prev-selector" v-on:click="selectPreviousRightArm()">&#9650;</button>
         <button class="next-selector" v-on:click="selectNextRightArm()">&#9660;</button>
-      </div>
+      </div> -->
     </div>
     <div class="bottom-row">
-      <div class="bottom part">
+      <PartSelector
+      :parts="availableParts.bases"
+      position="bottom"
+      @partSelected="part => selectedRobot.base = part" />
+      <!-- <div class="bottom part"> -->
         <!-- <img src="./images/base-single-wheel.png" title="left arm" /> -->
         <!-- <img v-bind:src="availableParts.bases[selectedBasesIndex].src" title="head" /> -->
-        <img v-bind:src="selectedRobot.base.src" title="base" />
+        <!-- <img v-bind:src="selectedRobot.base.src" title="base" />
         <button class="prev-selector" v-on:click="selectPreviousBases()">&#9668;</button>
         <button class="next-selector" v-on:click="selectNextBases()">&#9658;</button>
-      </div>
+      </div> -->
     </div>
     <div>
       <h1>Cart</h1>
@@ -76,6 +114,7 @@
 import availableParts from '../data/parts';
 import createdHookMixins from './created-hook-mixins';
 import PartSelector from './PartSelector.vue';
+import CollapsibleSection from '../shared/CollapsibleSection.vue';
 
 // function getPreviousValidIndex(index, length) {
 //   const deprecatedindex = index - 1;
@@ -89,7 +128,7 @@ import PartSelector from './PartSelector.vue';
 
 export default {
   name: 'RobotBuilder',
-  components: [PartSelector],
+  components: { PartSelector, CollapsibleSection },
   data() {
     return {
       availableParts,
@@ -334,8 +373,9 @@ export default {
 }
 .add-to-cart {
   position: absolute;
-  right: 30px;
-  width: 220px;
+  // right: 30px;
+  // width: 220px;
+  width: 210px;
   padding: 3px;
   font-size: 16px;
 }
@@ -350,5 +390,26 @@ th {
 }
 .sale-border {
   border: 3px solid red;
+}
+.preview {
+  position: absolute;
+  top: -20px;
+  right: 0;
+  width: 210px;
+  height: 210px;
+  padding: 5px;
+}
+.preview-content {
+  border: 1px solid #999;
+}
+.preview img {
+  width: 50px;
+  height: 50px;
+}
+.rotate-right {
+  transform: rotate(90deg);
+}
+.rotate-left {
+  transform: rotate(-90deg);
 }
 </style>
